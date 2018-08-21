bdfc90a66fcbb4099185fafb7707ca3319c1e8bb
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-08-20 20:12:35 -0400

    Adding in the header component along with some styles and resources along with the google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import './resources/styles.css';
+import Header from './components/header_footer/Header';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
