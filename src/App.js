33c22f54889dcd8e5d754317677ca5939f8e45e9
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-08-20 21:21:23 -0400

    Adding in the venue info information

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueNFO from './components/venue_nfo';
 
 class App extends Component {
   render() {
     return (
-      <div
-        className="App"
-        style={{ height: 1500, background: 'cornflowerblue' }}
-      >
+      <div className="App">
         <Header />
         <Featured />
+        <VenueNFO />
       </div>
     );
   }
