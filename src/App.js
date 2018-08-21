8b1cd724dce65845a3bca3d9d5824e032511e29e
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-08-20 20:52:39 -0400

    Adding in the image slider for the main images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         style={{ height: 1500, background: 'cornflowerblue' }}
       >
         <Header />
+        <Featured />
       </div>
     );
   }
