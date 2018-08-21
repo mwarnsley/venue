10a4163e057512f945725a96a47c329621217ae7
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-08-20 21:11:10 -0400

    updating the final date

--- src/components/featured/Countdown.js
+++ src/components/featured/Countdown.js
@@ -3,7 +3,7 @@ import Slide from 'react-reveal/Slide';
 
 class Countdown extends Component {
   state = {
-    deadline: 'Dec, 16, 2018',
+    deadline: 'March, 16, 2019',
     days: '0',
     hours: '0',
     minutes: '0',
